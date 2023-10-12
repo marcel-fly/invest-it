3957a2135ca5483c8562bc5b5f5cccb2e74e9c1b
marcel-fly <marcel9556@gmail.com>
2023-10-12 20:15:58 +0200

    Finish task

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,64 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchPhotosThunk, incrementPage } from "../reducers/app";
+
 const List = () => {
-  return <div style={{ minHeight: "90vh", width: "100%" }}></div>;
+  const photos = useSelector((state) => state.app.list);
+  const page = useSelector((state) => state.app.page);
+  const loading = useSelector((state) => state.app.loading);
+
+  const dispatch = useDispatch();
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    dispatch(fetchPhotosThunk({ perPage: 30, page }));
+  }, [page, dispatch]);
+
+  const handleScroll = () => {
+    if (
+      containerRef.current.scrollHeight - containerRef.current.scrollTop <=
+        containerRef.current.clientHeight + 200 &&
+      !loading
+    ) {
+      dispatch(incrementPage());
+    }
+  };
+
+  return (
+    <div
+      ref={containerRef}
+      onScroll={handleScroll}
+      style={{
+        width: "100vw",
+        height: "90vh",
+        overflowY: "scroll",
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 1fr)",
+      }}
+    >
+      {photos.map((photo) => (
+        <div
+          key={photo.id}
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            flexDirection: "column",
+            alignItems: "center",
+            height: "30vh",
+          }}
+        >
+          <img
+            style={{ marginTop: 0, height: "80%" }}
+            src={photo.urls.regular}
+            alt={photo.alt_description}
+          />
+
+          <p>{photo.user.name}</p>
+        </div>
+      ))}
+      {loading && <p>Loading...</p>}
+    </div>
+  );
 };
 
 export default List;
--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,9 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { fetchPhotos } from "../api";
 
 const initialState = {
   list: [],
+  page: 1,
+  loading: false,
+  error: null,
   darkMode: localStorage.getItem("dark-mode") === "true" || false,
 };
+
+export const fetchPhotosThunk = createAsyncThunk(
+  "app/fetchPhotos",
+  async (params) => {
+    const response = await fetchPhotos(params);
+    return response.images;
+  }
+);
 
 export const appSlice = createSlice({
   name: "app",
@@ -13,7 +25,24 @@ export const appSlice = createSlice({
       state.darkMode = !state.darkMode;
       localStorage.setItem("dark-mode", state.darkMode.toString());
     },
+    incrementPage: (state) => {
+      state.page += 1;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPhotosThunk.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchPhotosThunk.fulfilled, (state, action) => {
+        state.list = [...state.list, ...action.payload];
+        state.loading = false;
+      })
+      .addCase(fetchPhotosThunk.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
   },
 });
-export const { toggleDarkMode } = appSlice.actions;
+export const { incrementPage, toggleDarkMode } = appSlice.actions;
 export default appSlice.reducer;
